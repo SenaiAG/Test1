faa25d72900a4edefb7d5ba45026cc5e5c5f9f60
senaijava <48931722+senaijava@users.noreply.github.com>
2020-02-05 13:36:50 +0100

    Add files via upload

---
+++ ELE150.ino
@@ -0,0 +1,36 @@
+const unsigned long eventInterval=1000;
+unsigned long previousTime= 0;
+const int sensorPin=A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  unsigned long currentTime = millis();
+
+  if( currentTime - previousTime >= eventInterval){
+      getTemp();
+    previousTime=currentTime;
+  }
+ 
+}
+float getTemp(){
+  int sensorval = analogRead(sensorPin);
+  float voltage = (sensorval/1024.0)*5.0;
+  float temprature = (voltage - 0.5)*100;
+  
+  //consolWrite("Sensor :",sensorval); 
+  //consolWrite("Voltage :",voltage); 
+  consolWrite("Temprature :",temprature);
+ // Serial.println("*******************");
+  //delay(1000);
+return temprature;
+}
+
+void consolWrite(String navn,float value){
+ // Serial.print(navn);
+  Serial.println(value);
+}
+
